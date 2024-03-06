df16fbfce906ffb5f09688e88115e24ddb67002f
Shahid <mdshahid23456789@gmail.com>
2024-03-06 12:10:46 +0530

    jjj

---
+++ middleware/async.js
@@ -0,0 +1,13 @@
+const { CustomAPIError } = require("../errors/custom-error")
+
+const asyncWrapper = (fn) => {
+  return async (req, res, next) => {
+    try {
+      await fn(req, res, next)
+    } catch (error) {
+      next(error)
+    }
+  }
+}
+
+module.exports = asyncWrapper
